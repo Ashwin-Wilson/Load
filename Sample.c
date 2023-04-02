c05fcaf7c40f37e089f14ee6438489f60c5452ca
Ashwin Wilson <ashwinwilson24@gmail.com>
2023-04-02 17:21:35 +0000

    TwoFourTwentythree

--- Sample.c
+++ Sample.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
 void main(){
-    printf("Hello World");
+   for(i=num;i<=1;i++){
+    
+   }
 }
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+    char a[]="hello";
+    prints(a[]);
+}
+
+void prints(char a[]){
+    printf("%s",a[]);
+}
