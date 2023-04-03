54be9bea9a8edf5888a2e515238db74be06b14d6
Ashwin Wilson <ashwinwilson24@gmail.com>
2023-04-03 09:39:33 +0000

    pointNotRepeat

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,10 +2,15 @@ let Num=0, Opr=0,result=0;
 function Number(Num){
     document.getElementById("Display").value=document.getElementById("Display").value+Num;
 }
+function DisableBtn(){
+    document.getElementById("point").disabled = true;
+}
 function Operant(opr){
     Num=document.getElementById("Display").value;
     document.getElementById("Display").value="";
     Opr=opr; 
+    document.getElementById("point").disabled = false;
+
 }
 function Result(){
     console.log(Opr)
@@ -26,6 +31,7 @@ function Result(){
         result=Num-document.getElementById("Display").value;
         document.getElementById("Display").value=result;
     }
+    document.getElementById("point").disabled = false;
 }
 function clrScrn(){
     document.getElementById("Display").value="";
